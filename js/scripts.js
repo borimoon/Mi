aa4a036360761c0bd6926221c193b5afde1e88e1
borimoon <adrienn.ficzere@gmail.com>
2017-11-30 17:18:42 +0100

    header
    fix, responsive

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// header height resize
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > $('.game').outerHeight()){
+          $('.header').addClass("header-narrow");
+        }
+        else {
+          $('.header').removeClass("header-narrow");
+        }
+
+        if ($('.header').hasClass("header-narrow")){
+          $('.logo').attr("src", "assets/Mi-logo-s.svg");
+        }
+        else if ($('.header').removeClass("header-narrow")) {
+          $('.logo').attr("src", "assets/Mi-logo.svg");
+        }
+    });
+
+    // mobile menu show-hide
+    $(function() {
+    	var menuVisible = false;
+    	$('#toggleOn').click(function() {
+    		if (menuVisible) {
+    			$('#menu').css({'display':'none'});
+    			$('#toggleOn').removeClass('menuopen');
+    			menuVisible = false;
+    			return;
+    		}
+    		$('#menu').css({'display':'block'});
+    		$('#toggleOn').toggleClass('menuopen');
+    		menuVisible = true;
+    	});
+    	$('#menu').click(function() {
+    		$(this).css({'display':'none'});
+    		$('#toggleOn').removeClass('menuopen');
+    		menuVisible = false;
+    	});
+    });
