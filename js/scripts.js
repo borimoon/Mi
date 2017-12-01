eed47639a365b798802627c49ee79db8fda962e4
adriennficzere <boriana@Boriana.local>
2017-12-01 10:44:31 +0100

    menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
 // header height resize
+$(window).scroll(function() {
+    if ($(this).scrollTop() > $('.landing').offset().top){
+      $('.header').addClass("header-narrow");
+    }
+    else {
+      $('.header').removeClass("header-narrow");
+    }
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > $('.landing').outerHeight()){
-          $('.header').addClass("header-narrow");
-        }
-        else {
-          $('.header').removeClass("header-narrow");
-        }
+    if ($('.header').hasClass("header-narrow")){
+      $('.logo').attr("src", "assets/Mi-logo-s.svg");
+    }
+    else if ($('.header').removeClass("header-narrow")) {
+      $('.logo').attr("src", "assets/Mi-logo.svg");
+    }
+});
 
-        if ($('.header').hasClass("header-narrow")){
-          $('.logo').attr("src", "assets/Mi-logo-s.svg");
-        }
-        else if ($('.header').removeClass("header-narrow")) {
-          $('.logo').attr("src", "assets/Mi-logo.svg");
-        }
-    });
-
-    // mobile menu show-hide
-    $(function() {
-    	var menuVisible = false;
-    	$('#toggleOn').click(function() {
-    		if (menuVisible) {
-    			$('#menu').css({'display':'none'});
-    			$('#toggleOn').removeClass('menuopen');
-    			menuVisible = false;
-    			return;
-    		}
-    		$('#menu').css({'display':'block'});
-    		$('#toggleOn').toggleClass('menuopen');
-    		menuVisible = true;
-    	});
-    	$('#menu').click(function() {
-    		$(this).css({'display':'none'});
-    		$('#toggleOn').removeClass('menuopen');
-    		menuVisible = false;
-    	});
-    });
+// mobile menu show-hide
+$(function() {
+  var menuVisible = false;
+  $('#toggle').click(function() {
+    if (menuVisible) {
+      $('#menu').css({'display':'none'});
+      $('#toggle').removeClass('menuopen');
+      menuVisible = false;
+      return;
+    }
+    $('#menu').css({'display':'block'});
+    $('#toggle').toggleClass('menuopen');
+    menuVisible = true;
+  });
+  $('#menu').click(function() {
+    $(this).css({'display':'none'});
+    $('#toggle').removeClass('menuopen');
+    menuVisible = false;
+  });
+});
