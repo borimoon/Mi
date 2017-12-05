0b1744670d6b5660bd47b89956531d07613d87da
borimoon <adrienn.ficzere@gmail.com>
2017-12-05 17:57:27 +0100

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -16,23 +16,6 @@ $(document).ready(function (){
     });
 });
 
-// header height resize
-$(window).scroll(function() {
-    if ($(this).scrollTop() > $('.landing').offset().top){
-      $('.header').addClass("header-narrow");
-    }
-    else {
-      $('.header').removeClass("header-narrow");
-    }
-
-    if ($('.header').hasClass("header-narrow")){
-      $('.logo').attr("src", "assets/Mi-logo-s.svg");
-    }
-    else if ($('.header').removeClass("header-narrow")) {
-      $('.logo').attr("src", "assets/Mi-logo.svg");
-    }
-});
-
 // mobile menu show-hide
 $(function() {
   var menuVisible = false;
